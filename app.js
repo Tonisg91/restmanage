7571761262d17db7f2c2443d99d74bcb5bf3aa60
Tonisg91 <sancheztoni.garcia@gmail.com>
2020-09-12 13:30:08 +0200

    add middleware ruta

--- app.js
+++ app.js
@@ -74,6 +74,9 @@ app.use('/api/admin',
   menuRoutes,
   configRoutes
   )
-
+app.use((req, res, next) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + "/public/index.html");
+});
 
 module.exports = app;
