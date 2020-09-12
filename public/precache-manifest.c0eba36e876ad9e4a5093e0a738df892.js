0427d106b5c331d19243502c1b00dfac0d456d19
Tonisg91 <sancheztoni.garcia@gmail.com>
2020-09-12 13:11:19 +0200

    /api en rutas backend

--- public/precache-manifest.c0eba36e876ad9e4a5093e0a738df892.js
+++ public/precache-manifest.c0eba36e876ad9e4a5093e0a738df892.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f0b4cb742ffdd144af891589ead8a5b6",
+    "revision": "9b824675f4a3fdaa3689d87e108ab299",
     "url": "/index.html"
   },
   {
-    "revision": "02d0c7aa21bac43f1d27",
+    "revision": "d727f36261cebd341dc6",
     "url": "/static/css/2.d34346ea.chunk.css"
   },
   {
-    "revision": "ad612098460a8f316da9",
+    "revision": "4cd67d095f82ce483937",
     "url": "/static/css/main.f771b409.chunk.css"
   },
   {
-    "revision": "02d0c7aa21bac43f1d27",
-    "url": "/static/js/2.3474a30f.chunk.js"
+    "revision": "d727f36261cebd341dc6",
+    "url": "/static/js/2.bfe9ff2b.chunk.js"
   },
   {
     "revision": "eaaf63558baae456918a991a401208d7",
-    "url": "/static/js/2.3474a30f.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.bfe9ff2b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ad612098460a8f316da9",
-    "url": "/static/js/main.f70f719e.chunk.js"
+    "revision": "4cd67d095f82ce483937",
+    "url": "/static/js/main.a8fcf5d6.chunk.js"
   },
   {
     "revision": "055e94f6602794288cc3",
